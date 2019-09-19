5b6c6aa58507dea9d47f15b71bee95dacec71004
Clairezyw <31993027+Clairezyw@users.noreply.github.com>
2019-09-19 13:56:47 +0800

    add: shaka player

---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+export const manifestUri = 'https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd'
+
+export const licenseServer = 'https://cwip-shaka-proxy.appspot.com/wrapped_request'
